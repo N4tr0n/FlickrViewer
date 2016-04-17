aaf949ed3c480acaacbb8f4fcc674f7bc84b6dc8
Nathan Hitchings <nhitch@gmail.com>
2016-04-17 15:11:30 -0700

    Initial Commit

---
+++ FlickrViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrViewer
+//
+//  Created by Nathan Hitchings on 4/17/16.
+//  Copyright © 2016 N4TR0N. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
