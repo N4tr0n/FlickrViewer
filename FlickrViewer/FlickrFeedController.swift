a2f021c6f939ef62a1b364311400aea682f5379c
Nathan Hitchings <nhitch@gmail.com>
2016-04-20 19:51:42 -0700

    Add PhotoCell class

--- FlickrViewer/FlickrFeedController.swift
+++ FlickrViewer/FlickrFeedController.swift
@@ -11,19 +11,44 @@ import UIKit
 class FlickrFeedController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
   
   var collectionView: UICollectionView!
+  var photos: [Photo]?
   
   override func loadView() {
     collectionView = UICollectionView()
     view = collectionView
+    collectionView.registerClass(PhotoCell.self, forCellWithReuseIdentifier: "PhotoCell")
   }
   
   // MARK - <UICollectionViewDataSource>
   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 0;
+    if photos != nil {
+    return photos!.count;
+    } else {
+      return 0;
+    }
   }
 
   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-    return UICollectionViewCell()
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoCell", forIndexPath: indexPath)
+    return cell
+  }
+  
+  func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+    let photoCell = cell as? PhotoCell
+    if let photoCell = photoCell {
+      // set image
+    }
+  }
+  
+  func refreshPhotos(completion: (succeeded: Bool, photos: [Photo]?) -> ()) {
+    PhotoStore.sharedStore.loadPhotos { (success, photos) in
+      if success {
+        self.photos = photos
+        completion(succeeded: true, photos: photos)
+      } else {
+        completion(succeeded: false, photos: nil)
+      }
+    }
   }
 }
 
---
+++ FlickrViewer/PhotoCell.swift
@@ -0,0 +1,37 @@
+//
+//  PhotoCell.swift
+//  FlickrViewer
+//
+//  Created by Nathan Hitchings on 4/20/16.
+//  Copyright © 2016 N4TR0N. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+  var imageView: UIImageView!
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configureImageView()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    configureImageView()
+  }
+  
+  func configureImageView() {
+    imageView = UIImageView()
+    imageView.contentMode = .ScaleAspectFit
+    contentView.addSubview(imageView)
+    let topConstraint = imageView.topAnchor.constraintEqualToAnchor(contentView.topAnchor)
+    let bottomConstraint = imageView.bottomAnchor.constraintEqualToAnchor(contentView.bottomAnchor)
+    let leadingConstraint = imageView.leadingAnchor.constraintEqualToAnchor(contentView.leadingAnchor)
+    let trailingConstraint = imageView.trailingAnchor.constraintEqualToAnchor(contentView.trailingAnchor)
+    topConstraint.active = true
+    bottomConstraint.active = true
+    leadingConstraint.active = true
+    trailingConstraint.active = true
+  }
+}
